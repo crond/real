27906e79fc892ce1733594f5df12d58640d05e10
crondglobalname <crondglobalname@cms.com>
2012-12-31 07:49:13 +0530

    fifo test files added.The output from fifo.c.out  binary will do the following,        1. print the current policy and its priority range        2. change itself to FIFO policy and print its range

---
+++ fifo.c
@@ -0,0 +1,98 @@
+/*
+ * File name    : fifo.c
+ * Author       : crond
+ * email        : cronddaemon@gmail.com
+ * About File   : The output from this binary will do the following,
+ *      1. print the current policy and its priority range
+ *      2. change itself to FIFO policy and print its range
+ */
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <sched.h>
+#include <errno.h>
+#include <signal.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void sighandler(int signum){
+    printf("SIgnal :%d caught. \n");
+    if(signum == SIGSEGV)
+       _exit(0);
+    return;
+
+}
+int getrangeforschedpolicy(int policy){
+    
+    int min=0;
+    if((min=sched_get_priority_min(policy)) < 0){
+        printf("Error when getting min priority. Err: %d:%s\n",errno,strerror(errno));
+        return -1;
+    }    
+    int max=0;
+    if((max=sched_get_priority_max(policy)) < 0){
+        printf("Error when getting min priority. Err: %d:%s\n",errno,strerror(errno));
+        return -1;
+    }    
+    printf("\n For this policy, Priority values : Min:%d Max:%d\n",min,max);
+
+}
+int getschedpolicy(int pid){
+    int ret =0;
+    if((ret= sched_getscheduler(pid)) < 0){
+        printf("Error when getting sched policy. Err: %d:%s\n",errno,strerror(errno));
+        return -1;
+    }    
+    switch(ret){
+        case 0:
+            printf("Sched Type:Normal. Not running on any Priority  \n");
+            break;
+        case 1:
+            printf("Sched Type:FIFO\n");
+            break;
+        case 2:
+            printf("Sched Type:Round Robin Tim sharing Policy\n");
+            break;
+        case 3:
+            printf("Sched Type:Batch. Not running on any Priority. \n");
+            break;
+       default:
+            //Not supposed to come
+            printf("Sched Type:Unknown\n");
+            break;
+    }
+    return ret;
+}
+int main(int argc,char *argv[]){
+    int ret=0;
+    pid_t pid;
+    
+    signal(SIGSEGV,sighandler);
+    if(argc == 2){
+        char *buffer = (char *)malloc(strlen(argv[1]));
+        bzero(buffer,sizeof(buffer));
+        buffer = argv[1];
+        sscanf(buffer,"%d",&pid);
+    }
+    else
+        pid = getpid();
+    printf("This proc pid:%d.Getting Scheduler Type for PID:%d. \n",getpid(),pid);
+    int policy= getschedpolicy(pid);
+    getrangeforschedpolicy(policy);
+
+    //changing to FIFO schedule policy
+    struct sched_param schedPrity;
+    schedPrity.sched_priority=10;
+    errno =0;
+    if((ret = sched_setscheduler(getpid(),SCHED_FIFO,(const struct sched_param *)&schedPrity)) < 0){
+        printf("Error when setting sched algorithm. Err: %d:%s\n",errno,strerror(errno));
+        return -1;
+    
+    }  
+    policy= getschedpolicy(getpid());
+    getrangeforschedpolicy(policy);
+    return 0;
+
+}
+
